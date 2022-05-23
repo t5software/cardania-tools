4c72ca52a959f134d30549492defff0a13e67032
T5 Software <t5softwaretech@gmail.com>
2022-05-23 18:31:20 +0000

    add tip button #4

--- tests/test.js
+++ tests/test.js
@@ -4,6 +4,11 @@ import { expect, test } from '@playwright/test';
 // Be sure to set it to a value to run npm test locally
 const baseUrl = '/' + process.env.TARGET.split('/').pop();
 
+// allow clipboard permissions
+test.use({
+	permissions: ['clipboard-read', 'clipboard-write']
+});
+
 test('index page has expected h1', async ({ page }) => {
 	await page.goto(baseUrl);
 	expect(await page.textContent('h1')).toBe('What is this?');
@@ -46,4 +51,20 @@ test('search address with all resources expect no missing summary', async ({ pag
 	expect((await page.$$('.green-check')).length === 40);
 	expect(((await page.$$('.red-ex'))?.length ?? 0) === 0);
 	expect(await page.textContent('#summary')).toBe('No Cardania Resources missing!');
+});
+
+test('check click to copy button', async ({ page }) => {
+	await page.goto(baseUrl);
+
+	// read clipboard initially for blank
+	const clipboard1 = await page.evaluate(() => navigator.clipboard.readText());
+	expect(clipboard1).not.toBeTruthy();
+
+	await page.click('#addressButton');
+
+	const clipboard2 = await page.evaluate(() => navigator.clipboard.readText());
+
+	expect(clipboard2).toBe(
+		'addr1qy047pejncekqrqm8qu33hx3sjzpv4vh4lal3ud906qnn44a6vchc2wrqstx29vnzfe58swdae7jczh2hj6kam3nf5zs5gfhan'
+	);
 });
